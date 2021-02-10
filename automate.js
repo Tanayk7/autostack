24971b1d9806ac8df3f5e49c0c2c28dedb6e8de3
MSI\Tanay <tanaykulkarni7@gmail.com>
2021-02-10 12:16:31 +0530

    updated code for automation test

---
+++ automate.js
@@ -0,0 +1,12 @@
+// const commands = require('./lib/commands');
+// const files = require('./lib/files');
+
+// try{
+//     commands.execute_command("autostack");
+// }
+// catch(err){
+//     console.log(err)
+// }
+
+// console.log(__dirname + "/components");
+// let common_components = files.list_files(__dirname + '/components');
--- autostack.js
+++ autostack.js
@@ -3,14 +3,14 @@ const codegen = require('./lib/codegen');
 
 (async () => {
     await codegen.setup_config();
-    codegen.create_react_app();
-    codegen.clean_react_app();
-    codegen.create_app_directories();
-    codegen.generate_app_routes();
-    codegen.generate_app_components();
-    codegen.generate_app_state();
-    codegen.install_dependencies();
-    codegen.start_server();
+    // codegen.create_react_app();
+    // codegen.clean_react_app();
+    // codegen.create_app_directories();
+    // codegen.generate_app_routes();
+    // codegen.generate_app_components();
+    // codegen.generate_app_state();
+    // codegen.install_dependencies();
+    // codegen.start_server();
 })();
 
 
